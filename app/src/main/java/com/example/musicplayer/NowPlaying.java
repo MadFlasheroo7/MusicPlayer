1fa7e44cf6bd1f944d67ab812f69ad89b756924f
jayesh_seth <jayeshseth45@gmail.com>
2020-07-19 01:05:00 +0530

    added get music

--- app/src/main/java/com/example/musicplayer/NowPlaying.java
+++ app/src/main/java/com/example/musicplayer/NowPlaying.java
@@ -9,6 +9,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.media.Image;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -28,10 +29,36 @@ import java.util.TimerTask;
 
 public class NowPlaying extends AppCompatActivity {
 
+    TextView song_name, artist_name, duration_played, duration_total;
+    ImageButton nxtBtn, prevBtn, playPauseBtn, repeatBtn, shuffleBtn;
+    ImageView cover_art, vol_up, vol_down;
+    SeekBar track_seek, seek_vol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_now_playing);
+        initViews();
     }
+
+    private void initViews() {
+        song_name = findViewById(R.id.titleTrack);
+        artist_name = findViewById(R.id.song_artist);
+        duration_played = findViewById(R.id.trackDuration);
+        duration_total = findViewById(R.id.trackDuration_total);
+
+        nxtBtn = findViewById(R.id.nxtBtn);
+        prevBtn = findViewById(R.id.previousBtn);
+        playPauseBtn = findViewById(R.id.play_pause_Btn);
+        repeatBtn = findViewById(R.id.repeatBtn);
+        shuffleBtn = findViewById(R.id.shuffleBtn);
+
+        cover_art = findViewById(R.id.coverArt);
+        vol_up = findViewById(R.id.volUp);
+        vol_down = findViewById(R.id.volDown);
+
+        track_seek = findViewById(R.id.seek_Track);
+        seek_vol = findViewById(R.id.seek_Vol);
+    }
+
 }
