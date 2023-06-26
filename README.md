5aaf67ddaa37aa1b8e5c2680631472104f79ee37
MadFlasheroo7 <jayeshseth45@gmail.com>
2023-06-26 15:21:42 +0530

    add readme and screenshots

---
+++ README.md
@@ -0,0 +1,33 @@
+# Music Player
+Music Player is my first ever app written in (java and xml) it was first time learning git, android app development and so much more. The code isn't clean, file structure is pretty bad, app is laggy but it works so 😊 👍.
+
+# Things I Learned
+- Java
+- XML
+- Lottie
+- Motion Layout and Animations
+
+# Screenshots
+## Splash Screen
+|          Splash Screen 1          |          Splash Screen 2          |          Splash Screen 3          |
+|:---------------------------------:|:---------------------------------:|:---------------------------------:|
+| ![](./screenshots/spalsh%201.png) | ![](./screenshots/spalsh%202.png) | ![](./screenshots/spalsh%203.png) |
+
+## Light Mode
+| All Songs                                  | Navigation Drawer                           | Albums                               | About Me                                  | About Me Scrolled                             |
+|--------------------------------------------|---------------------------------------------|--------------------------------------|-------------------------------------------|-----------------------------------------------|
+| ![](./screenshots/all%20songs%20light.png) | ![](./screenshots/nav%20drawer%20light.png) | ![](./screenshots/album%20light.png) | ![](./screenshots/about%20me%20light.png) | ![](./screenshots/about%20me%202%20light.png) |
+
+## Dark Mode
+| All Songs                          | Navigation Drawer                          | Albums                              | About Me                     | About Me Scrolled                     |
+|------------------------------------|--------------------------------------------|-------------------------------------|------------------------------|---------------------------------------|
+| ![](./screenshots/all%20songs.png) | ![](./screenshots/nav%20drawer.png)        | ![](./screenshots/album.png)        | ![](./screenshots/about.png) | ![](./screenshots/about%20me%202.png) |
+
+# Credits
+- Google
+- Youtube
+- Github
+- Stackoverflow
+
+# Thank you
+A Big Thank you for stumbling upon this old repo of mine. I don't know how and why you came here but anyways have a good day. and do not read the code please :)
--- app/src/main/java/com/example/musicplayer/Settings_Activity.java
+++ app/src/main/java/com/example/musicplayer/Settings_Activity.java
@@ -1,91 +1,91 @@
 package com.example.musicplayer;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.preference.Preference;
-import androidx.preference.PreferenceFragmentCompat;
-import androidx.preference.PreferenceManager;
-import androidx.preference.SwitchPreferenceCompat;
-
-import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.os.Bundle;
-
-public class Settings_Activity extends AppCompatActivity {
-
-    SharedPreferences sharedPreferences;
-    SharedPreferences.Editor editor;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-        Settings();
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.userProfile, new ProfileSettings())
-                .commit();
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.themeSettings, new ThemeSettings())
-                .commit();
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.abtSettings , new AbtSettings())
-                .commit();
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.updateSettings , new updateSettings())
-                .commit();
-    }
-    public static class ProfileSettings extends PreferenceFragmentCompat {
-        @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.profile_preferences, rootKey);
-        }
-    }
-    public void Settings(){
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-    }
-    public static class ThemeSettings extends PreferenceFragmentCompat {
-        @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.theme_preferences, rootKey);
-//            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-            final SwitchPreferenceCompat ModeSwitch = findPreference("darkMode");
-            assert ModeSwitch != null;
-            ModeSwitch.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-                @Override
-                public boolean onPreferenceClick(Preference preference) {
-//                    ModeSwitch.setSummaryOn();
-                    if (ModeSwitch.isChecked()){
-                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                    }else {
-                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                    }
-                    return false;
-                }
-            });
-            SwitchPreferenceCompat BlackMode = findPreference("blackMode");
-            assert BlackMode != null;
-            BlackMode.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-                @Override
-                public boolean onPreferenceClick(Preference preference) {
-                    return false;
-                }
-            });
-        }
-    }
-    public static class AbtSettings extends PreferenceFragmentCompat {
-        @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.abt_preferences, rootKey);
-        }
-    }
-    public static class updateSettings extends PreferenceFragmentCompat {
-        @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.update_preferences, rootKey);
-        }
-    }
-}
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.app.AppCompatDelegate;
+//import androidx.constraintlayout.widget.ConstraintLayout;
+//import androidx.preference.Preference;
+//import androidx.preference.PreferenceFragmentCompat;
+//import androidx.preference.PreferenceManager;
+//import androidx.preference.SwitchPreferenceCompat;
+//
+//import android.content.SharedPreferences;
+//import android.graphics.Color;
+//import android.os.Bundle;
+//
+//public class Settings_Activity extends AppCompatActivity {
+//
+//    SharedPreferences sharedPreferences;
+//    SharedPreferences.Editor editor;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_settings);
+//        Settings();
+//        getSupportFragmentManager()
+//                .beginTransaction()
+//                .replace(R.id.userProfile, new ProfileSettings())
+//                .commit();
+//        getSupportFragmentManager()
+//                .beginTransaction()
+//                .replace(R.id.themeSettings, new ThemeSettings())
+//                .commit();
+//        getSupportFragmentManager()
+//                .beginTransaction()
+//                .replace(R.id.abtSettings , new AbtSettings())
+//                .commit();
+//        getSupportFragmentManager()
+//                .beginTransaction()
+//                .replace(R.id.updateSettings , new updateSettings())
+//                .commit();
+//    }
+//    public static class ProfileSettings extends PreferenceFragmentCompat {
+//        @Override
+//        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+//            setPreferencesFromResource(R.xml.profile_preferences, rootKey);
+//        }
+//    }
+//    public void Settings(){
+//        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+//    }
+//    public static class ThemeSettings extends PreferenceFragmentCompat {
+//        @Override
+//        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+//            setPreferencesFromResource(R.xml.theme_preferences, rootKey);
+////            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+//            final SwitchPreferenceCompat ModeSwitch = findPreference("darkMode");
+//            assert ModeSwitch != null;
+//            ModeSwitch.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+//                @Override
+//                public boolean onPreferenceClick(Preference preference) {
+////                    ModeSwitch.setSummaryOn();
+//                    if (ModeSwitch.isChecked()){
+//                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//                    }else {
+//                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//                    }
+//                    return false;
+//                }
+//            });
+//            SwitchPreferenceCompat BlackMode = findPreference("blackMode");
+//            assert BlackMode != null;
+//            BlackMode.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+//                @Override
+//                public boolean onPreferenceClick(Preference preference) {
+//                    return false;
+//                }
+//            });
+//        }
+//    }
+//    public static class AbtSettings extends PreferenceFragmentCompat {
+//        @Override
+//        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+//            setPreferencesFromResource(R.xml.abt_preferences, rootKey);
+//        }
+//    }
+//    public static class updateSettings extends PreferenceFragmentCompat {
+//        @Override
+//        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+//            setPreferencesFromResource(R.xml.update_preferences, rootKey);
+//        }
+//    }
+//}
